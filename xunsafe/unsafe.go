57e661aa8e0df57a1092ee64e8058a1a236e62ac
lwgojustgo <lwgojustgo@163.com>
2024-11-22 14:38:00 +0800

    update

--- xunsafe/unsafe.go
+++ xunsafe/unsafe.go
@@ -1,5 +1,6 @@
 package xunsafe
 
+import "C"
 import (
 	"reflect"
 	"unsafe"
@@ -15,4 +16,36 @@ func TestPanicFunc() {
 	}
 	b := *(*[]byte)(unsafe.Pointer(&bh))
 	b[0] = 'H'
+}
+
+func String2Bytes(s string) []byte {
+	x := (*[2]uintptr)(unsafe.Pointer(&s))
+	h := [3]uintptr{x[0], x[1], x[1]}
+	return *(*[]byte)(unsafe.Pointer(&h))
+}
+
+func Bytes2String(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+func AToByteHelp(v string) []byte {
+	x := (*[2]uintptr)(unsafe.Pointer(&v))
+	h := [3]uintptr{x[0], x[1], x[1]}
+	return *(*[]byte)(unsafe.Pointer(&h))
+}
+
+func GoStringToCString(logPath string) C.CString {
+	return C.CString(logPath)
+}
+
+func ByteArrayToCString(ba []byte) *C.char {
+	ba = append(ba, 0) // 防止转成C++的string后，末尾没有结束符，导致C++判断失误
+	s := Bytes2String(ba)
+	p := (*reflect.SliceHeader)(unsafe.Pointer(&s))
+	return (*C.char)(unsafe.Pointer(p.Data))
+}
+
+func UIntCToGo(ptr unsafe.Pointer) uint32 {
+	bodySize := uint32(*(*C.uint)(ptr))
+	return bodySize
 }
